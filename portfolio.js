2a137d7df6b65bc3e8859fa9cd6170b89fd4e61e
Monan Leal <monanleal@outlook.com>
2024-04-02 18:32:23 -0300

    .

--- portfolio.js
+++ portfolio.js
@@ -28,12 +28,12 @@ sr.reveal('#sobreMim', {duration: 2000, delay:200, distance:"1000px", origin:'le
 sr.reveal('#habilidades', {duration: 3000 });
 
 sr.reveal('#habilidades', {duration: 3000 });
-sr.reveal('.caixas', {duration: 2000 });
+sr.reveal('.caixas', {duration: 2500 });
 sr.reveal('.projetos', {duration: 3000 });
 
-sr.reveal('.caixa2P1', {rotate: {x: 0, y: 900, z:0 }, duration: 1500, delay: 1100, origin:'left', distance:'1000px'});
-sr.reveal('.caixa2P2', {rotate: {x: 0, y: 900, z:0 }, duration: 1500, delay: 700, origin:'left', distance:'1000px'});
-sr.reveal('.caixa2P3', {rotate: {x: 0, y: 500, z:0 }, duration: 1500, delay: 300, origin:'left', distance:'1000px'});
+sr.reveal('.caixa2P1', {rotate: {x: 0, y: 90, z:0 }, duration: 1500, delay: 300, origin:'left', distance:'1000px'});
+sr.reveal('.caixa2P2', {rotate: {x: 0, y: 90, z:0 }, duration: 1500, delay: 700, origin:'left', distance:'1000px'});
+sr.reveal('.caixa2P3', {rotate: {x: 0, y: 90, z:0 }, duration: 1500, delay: 1100, origin:'left', distance:'1000px'});
 
 sr.reveal('.projeto1', {rotate: {x: 0, y:0, z:0 }, duration: 1500, delay: 300, distance:'900px'});
 sr.reveal('.projeto2', {rotate: {x: 0, y:0, z:0 }, duration: 1500, delay: 500, distance:'900px'});
